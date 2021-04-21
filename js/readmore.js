e3a43723e9478943ec641a79868f65b809d2a295
Víctor <42402204+VictorLopezH@users.noreply.github.com>
2021-04-22 00:53:19 +0200

    Add files via upload

--- js/readmore.js
+++ js/readmore.js
@@ -3,10 +3,12 @@ document.querySelector('#readmore-button').addEventListener('click',function(){
     if(!i){
         document.getElementById('readMore').style.display='inline';
         document.getElementById('readmore-button').innerHTML='Mostrar menos <i class="icono-readmore fas fa-angle-up fa-lg"></i>';
+        document.getElementById('ultimo-readMore').classList.remove('ultimo-readMore');
         i=1;
     }else{
         document.getElementById('readMore').style.display='none';
         document.getElementById('readmore-button').innerHTML='...Mostrar más experiencias <i class="icono-readmore fas fa-angle-down fa-lg"></i>';
+        document.getElementById('ultimo-readMore').classList.add('ultimo-readMore');
         i=0;
     }
 })
